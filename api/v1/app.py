dca35913b57413ff4bfe3f482354b19f365bf227
robertglatzel <robertglatzel@gmail.com>
2019-02-05 23:27:56 +0000

    Created 404 json response instead of the defualt 404 html response

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Flask app
 """
 
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,8 +15,6 @@ app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
-
-
 @app.teardown_appcontext
 def cleanup(func):
     """ This function calls the close method in storage
@@ -24,6 +22,13 @@ def cleanup(func):
     """
 
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Returns a json error message instead of an html error page"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     hbnb_host = getenv("HBNB_API_HOST")
