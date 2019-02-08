ece2839ea46c6744cb9c83a9fc264b1237457bf9
VortexH <hemant.heer@gmail.com>
2019-02-08 23:07:24 +0000

    task 14 api call completed

--- api/v1/views/place_amenities.py
+++ api/v1/views/place_amenities.py
@@ -20,19 +20,31 @@ def retrieve_amenity_objects(place_id):
 
     if request.method == 'GET':
         amenities_dicts = [single_amenity.to_dict() for single_amenity
-                in single_place.amenities]
+                           in single_place.amenities]
         return jsonify(amenities_dicts)
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE', 'POST'])
-def specific_amenity(place_id, amenity_id):
 
+@app_views.route(
+        '/places/<place_id>/amenities/<amenity_id>',
+        methods=['DELETE', 'POST']
+        )
+def specific_amenity(place_id, amenity_id):
+    """ Handles the DELETE and POST HTTP methods
+
+        DELETE:
+            Deletes an amenity object from the db
+
+        POST:
+            Either creates a new instance of amenity with the link to a place
+            or returns an existing instance with a link to the place_id
+    """
     place = storage.get("Place", place_id)
     amenity = storage.get("Amenity", amenity_id)
     if place is None or amenity is None:
         abort(404)
 
+    amenities_obj_list = place.amenities
     if request.method == 'DELETE':
-        amenities_obj_list = place.amenities
         for amenity_obj in amenities_obj_list:
             if amenity_obj.id == amenity_id:
                 storage.delete(amenity_obj)
@@ -46,14 +58,11 @@ def specific_amenity(place_id, amenity_id):
         abort(404)
 
     if request.method == 'POST':
-        json_dict = request.get_json()
-        if json_dict is None:
-            return make_response(jsonify({"error": "Not a JSON"}), 400)
-        ignore_keys = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
-        for key, value in json_dict.items():
-            if key not in ignore_keys:
-                setattr(review, key, value)
-
-        storage.new(review)
-        storage.save()
-        return make_response(jsonify(review.to_dict()), 200)
+        if amenity in amenities_obj_list:
+            return make_response(jsonify(amenity.to_dict()), 200)
+        if storage_t == 'db':
+            place.amenities.append(amenity)
+        else:
+            place.amenity_ids.append(amenity.id)
+        place.save()
+        return make_response(jsonify(amenity.to_dict()), 201)
